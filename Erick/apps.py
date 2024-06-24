eae6114771c742367a276b3338ebe687cc9b48bc
ErickFer69 <erickcarrenov.est@uens.edu.ec>
2024-06-24 01:53:04 +0000

    Initial commit

--- Erick/apps.py
+++ Erick/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SurvivorcompassConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "survivorcompass"
+    name = "Erick"
